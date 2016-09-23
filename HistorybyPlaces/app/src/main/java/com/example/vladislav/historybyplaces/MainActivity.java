0a329a50a0dc3e0e380179d1d5b5678b5a57fc9f
Vladislav Titov <titovvlad1997@yandex.ru>
2016-09-23 18:27:03 +0300

    with music and preferences

--- HistorybyPlaces/app/src/main/java/com/example/vladislav/historybyplaces/MainActivity.java
+++ HistorybyPlaces/app/src/main/java/com/example/vladislav/historybyplaces/MainActivity.java
@@ -1,17 +1,27 @@
 package com.example.vladislav.historybyplaces;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Typeface;
+import android.media.MediaPlayer;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     TextView tv_play;
     TextView tv_records;
     TextView tv_options;
+
+    static MediaPlayer mp;
+
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +31,15 @@ public class MainActivity extends AppCompatActivity {
         tv_play = (TextView) findViewById(R.id.start);
         tv_records = (TextView) findViewById(R.id.tv_records);
         tv_options = (TextView) findViewById(R.id.tv_options);
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        mp = MediaPlayer.create(this, R.raw.ktr);
+        mp.setLooping(true);
+        mp.setVolume(100, 100);
+        mp.start();
+        if (!sharedPreferences.getBoolean("sp_volume", false)){
+            mp.pause();
+        }
 
         Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/CormorantInfant-Regular.ttf");
         tv_play.setTypeface(tf);
@@ -34,5 +53,36 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        tv_options.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        sharedPreferences.registerOnSharedPreferenceChangeListener(new SharedPreferences.OnSharedPreferenceChangeListener() {
+            @Override
+            public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+                if (s.equals("sp_volume")){
+                    if (!sharedPreferences.getBoolean("sp_volume", false)){
+                        mp.pause();
+                    }else{
+                        mp.start();
+                    }
+                }
+            }
+        });
+
     }
+
+    @Override
+    protected void onDestroy() {
+        mp.stop();
+        mp.release();
+        super.onDestroy();
+    }
+
 }
---
+++ HistorybyPlaces/app/src/main/java/com/example/vladislav/historybyplaces/SettingsActivity.java
@@ -0,0 +1,22 @@
+package com.example.vladislav.historybyplaces;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.SeekBar;
+import android.widget.Switch;
+import android.widget.TextView;
+
+public class SettingsActivity extends PreferenceActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
+
+
+    }
+}
